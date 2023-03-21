cfd153a9306cac47a7c33e2ff52288cbd2f4764a
ParadoxV5 <paradox.ver5@gmail.com>
2023-03-21 01:10:34 -0600

    fix `Mob#setup_position` Y-positions

--- mob/mob.gd
+++ mob/mob.gd
@@ -9,7 +9,8 @@ signal squashed
 func _physics_process(_delta) -> void: move_and_slide()
 
 func setup_position(start_position: Vector3, player_position: Vector3):
-  look_at_from_position(start_position, player_position, Vector3.UP)
+  player_position.y = position.y
+  look_at_from_position(position + start_position, player_position, Vector3.UP)
   rotate_y(deg_to_rad(randf_range(-rotate_range, rotate_range)))
   velocity = (
     Vector3.FORWARD * randf_range(min_speed, max_speed)

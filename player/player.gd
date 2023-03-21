6eb3f01c9a6d6abd3f78c13fb7a4172459582a98
ParadoxV5 <paradox.ver5@gmail.com>
2023-03-20 18:03:32 -0600

    code Player jump

--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
 ## Horizontal Speed (m/s)
-@export var speed: float = 14
-## Acceleration due to Gravity (m/s²).
-@export var fall_acc = 75
+@export var speed: float
+## Jump Impulse Speed (m/s)
+@export var jump_impulse: float = 20
+## Acceleration due to Gravity (m/s²)
+@export var fall_acc: float = 75
 
 func _physics_process(delta: float) -> void:
   ## 2D direction on the horizontal plane
@@ -12,13 +14,17 @@ func _physics_process(delta: float) -> void:
     &"move_forward", &"move_back"
   ) * speed
   var velocity2: Vector3 = Vector3(direction.x, 0, direction.y)
-  
-  if !is_on_floor():
-    # Fall by gravity
-    velocity2.y = velocity.y - fall_acc * delta
-  velocity = velocity2
+    # Currently only a 3D horizontal-only direction
   
   if direction != Vector2.ZERO:
-    $Pivot.look_at(position + velocity)
+    $Pivot.look_at(position + velocity2)
+  
+  # Find the Y component of `velocity2` and apply it
+  if is_on_floor():
+    if Input.is_action_just_pressed(&"jump"):
+      velocity2.y = jump_impulse
+  else:
+    velocity2.y = velocity.y - fall_acc * delta # Fall by gravity
+  velocity = velocity2
   
   move_and_slide()
